ee9f5be5fa544d0254e211193cab91d1a730da79
Eric Zhou <42453465+TheZooo@users.noreply.github.com>
2019-04-02 12:31:36 -1000

    Update p5safePlatScript.js

--- p5safePlatScript.js
+++ p5safePlatScript.js
@@ -5,32 +5,4 @@ function safePlatform() {
     fill(color('green'));
     rect(this.x,this.y,safePlatSize,safePlatSize);
   };
-  this.update = function() {
-    collision = collideRectCircle(this.x,this.y,safePlatSize,safePlatSize,jumper.x,jumper.y,jumperSize);
-    if (collision === true) {
-      if (this.x < jumper.x) {
-        jumper.x = this.x;
-        jumper.travel = 0;
-      }
-      else {
-        jumper.travel = 5;
-      }
-      if (this.x > jumper.x) {
-        jumper.travel = 0;
-      }
-      else {
-        jumper.travel = 5;
-      }
-      if (this.y < jumper.y) {
-        jumper.gravity = 0;
-        jumper.velocity = 0;
-      }
-      else {
-        jumper.gravity = 2;
-      }
-      if (this.y > jumper.y) {
-        jumper.velocity = 0;
-      }
-    }
-  };
 }
